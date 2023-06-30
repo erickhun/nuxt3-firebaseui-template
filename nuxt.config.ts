be06792a8dd471b64a56c39afbaf8b01704860a5
Eric <enregistrements@gmail.com>
2023-06-30 15:24:33 +0200

    firebase config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  // we deactivate the Server Side Rendering
+  ssr: false,
+
+  // Add nuxt-vuefire module
+  modules: [
+    'nuxt-vuefire',
+  ],
+
+  vuefire: {
+
+    auth: true, // enable Firebase Authentication
+
+    config: {
+
+      // replace with your own credentials from the Firebase console
+      apiKey: '...',
+      authDomain: '...',
+      projectId: '...',
+      appId: '...',
+    },
+  },
+
 })
